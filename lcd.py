507dc893d75542c42c60fd20dc522c7fdf864109
Abhignanendra Penugonda <91119502+AbhignanendraP@users.noreply.github.com>
2024-11-04 23:39:47 +0530

    Add files via upload

--- lcd.py
+++ lcd.py
@@ -2,25 +2,43 @@ import time
 from RPLCD.gpio import CharLCD
 import RPi.GPIO as GPIO
 
+trig=23
+echo=24
+
 # Define LCD setup
 lcd = CharLCD(
     numbering_mode=GPIO.BCM,
     cols=16,
     rows=2,
-    pin_rs=7,
-    pin_e=8,
-    pins_data=[25, 24, 23, 18]
+    pin_rs=26,
+    pin_e=19,
+    pins_data=[13,6,5,11]
 )
 
-# Write text to LCD
-lcd.write_string("Hello, World!")
-time.sleep(2)
+GPIO.setup(trig,GPIO.OUT)
+GPIO.setup(echo,GPIO.IN)
 
-# Clear the display
-lcd.clear()
-lcd.write_string("Welcome to Pi!")
-time.sleep(2)
+try:
+	while True:
+		GPIO.output(trig,True)
+		time.sleep(0.00001)
+		GPIO.output(trig,False)
+		
+		while GPIO.input(echo)==0:
+			start_time=time.time()
+		while GPIO.input(echo)==1:
+			end_time=time.time()
+		duration = end_time -start_time
+		distance = (duration*34300)/2
+		
+		
+		lcd.write_string(f"Dist : {distance:.2f} cm")	
+		
+		print(distance)	
+		time.sleep(1)
+		lcd.clear()
 
-# Clean up
-lcd.clear()
-GPIO.cleanup()
+except KeyboardInterrupt:
+	print('stopped')
+	lcd.clear()
+	GPIO.cleanup()
